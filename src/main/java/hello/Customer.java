5d82a7a217a8f71aacf02c6da0b6b6c606a9485f
VasylNyzhnyk <VasylNyzhnyk@users.noreply.github.com>
2019-04-02 17:07:21 +0300

    connection

--- src/main/java/hello/Customer.java
+++ src/main/java/hello/Customer.java
@@ -5,8 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
-public class Customer {
+    @Entity
+    public class Customer {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
--- src/main/java/hello/CustomerRepository.java
+++ src/main/java/hello/CustomerRepository.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
     List<Customer> findByLastName(String lastName);
+    
 }
